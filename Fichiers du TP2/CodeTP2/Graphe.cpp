04a08455fb0e20d6774d39972bb6f57e03d04d5b
nyx <45569313+nnyx-it@users.noreply.github.com>
2020-03-08 19:29:20 -0400

    Add files via upload

---
+++ Fichiers du TP2/CodeTP2/Graphe.cpp
@@ -0,0 +1,21 @@
+/**
+* \file Graphe.cpp
+* \brief Implémentation d'un graphe orienté.
+* \author Étudiant(e)
+* \version 0.1
+* \date mars 2020
+*
+*  Travail pratique numéro 2
+*
+*/
+
+#include "Graphe.h"
+
+//vous pouvez inclure d'autres librairies si c'est nécessaire
+
+namespace TP2
+{
+
+	//Mettez l'implémentation de vos méthodes ici.
+
+}//Fin du namespace
---
+++ Fichiers du TP2/CodeTP2/ReseauAerien.cpp
@@ -0,0 +1,83 @@
+/**
+ * \file ReseauAerien.cpp
+ * \brief Implémentattion de la classe ReseauAerien.
+ * \author Étudiant(e)
+ * \version 0.1
+ * \date mars 2020
+ *
+ *  Travail pratique numéro 2
+ *
+ */
+
+#include "ReseauAerien.h"
+#include <sstream>
+#include <fstream>
+//vous pouvez inclure d'autres librairies si c'est nécessaire
+
+namespace TP2
+{
+
+// Méthode fournie
+/**
+ * \fn void ReseauAerien::chargerReseau(std::ifstream & fichierEntree)
+ * \param[in] le fichier contenant l'information sur le réseau
+ */
+    void ReseauAerien::chargerReseau(std::ifstream & fichierEntree)
+    {
+        if (!fichierEntree.is_open())
+            throw std::logic_error("ReseauAerien::chargerReseau: Le fichier n'est pas ouvert !");
+
+        std::string buff;
+        getline(fichierEntree, nomReseau);
+        nomReseau.erase(0, 15);
+
+        int nbVilles;
+
+        fichierEntree >> nbVilles;
+        getline(fichierEntree, buff); //villes
+
+        unReseau.resize(nbVilles);
+
+        getline(fichierEntree, buff); //Liste des villes
+
+        size_t i = 0;
+
+        getline(fichierEntree, buff); //Premiere ville
+
+        while(buff != "Liste des trajets:")
+        {
+            std::string ville = buff;
+
+            unReseau.nommer(i, ville);
+
+            getline(fichierEntree, buff);
+            i++;
+        }
+
+        while(!fichierEntree.eof())
+        {
+            getline(fichierEntree, buff);
+            std::string source = buff;
+
+            getline(fichierEntree, buff);
+            std::string destination = buff;
+
+            getline(fichierEntree, buff);
+            std::istringstream iss(buff);
+
+            float duree;
+            iss >> duree;
+
+            float cout;
+            iss >> cout;
+
+            int ns;
+            iss >> ns;
+
+            unReseau.ajouterArc(unReseau.getNumeroSommet(source), unReseau.getNumeroSommet(destination), duree, cout, ns);
+        }
+    }
+
+    //À compléter par l'implémentation des autres méthodes demandées
+
+}//Fin du namespace
