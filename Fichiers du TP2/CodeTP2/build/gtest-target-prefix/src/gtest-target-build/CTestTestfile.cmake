ee7e75649c5385cbf6c22016cad4e3a6ae0f8d77
bryan-elliott.tam.1@ulaval.ca <bryan-elliott.tam.1@ulaval.ca>
2020-03-12 08:17:29 -0400

    cmakelist config

--- Fichiers du TP2/CodeTP2/build/gtest-target-prefix/src/gtest-target-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilyes/projects/tp2/tp2-ift-2008/Fichiers du TP2/CodeTP2/build/gtest-target-prefix/src/gtest-target
-# Build directory: /home/ilyes/projects/tp2/tp2-ift-2008/Fichiers du TP2/CodeTP2/build/gtest-target-prefix/src/gtest-target-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
