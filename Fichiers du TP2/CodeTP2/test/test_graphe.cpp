52f9179ce1d2f77539af53417d35937c1c85119f
bryan-elliott.tam.1@ulaval.ca <bryan-elliott.tam.1@ulaval.ca>
2020-03-15 00:14:33 -0400

    simply some assert

--- Fichiers du TP2/CodeTP2/test/test_graphe.cpp
+++ Fichiers du TP2/CodeTP2/test/test_graphe.cpp
@@ -105,7 +105,6 @@ protected:
         graph_n_Arc->ajouterArc(source2, destination2, duree2, cout2, ns2);
         graph_n_Arc->ajouterArc(source3, destination3, duree3, cout3, ns3);
         graph_n_Arc->ajouterArc(source4, destination4, duree4, cout4, ns4);
-        
     }
     virtual void TearDown()
     {
@@ -117,7 +116,7 @@ protected:
 
         graph_2_Arc = nullptr;
         delete graph_2_Arc;
-        
+
         graph_n_Arc = nullptr;
         delete graph_n_Arc;
     }
@@ -169,7 +168,7 @@ TEST_F(Studs, GivenAGraphWhenAjouterArcThisArcShouldExistWithTheRightParameter)
     ASSERT_EQ(graphVide->getPonderationsArc(source, destination).cout, cout);
     ASSERT_EQ(graphVide->getPonderationsArc(source, destination).ns, ns);
 
-    ASSERT_EQ(graphVide->arcExiste(source,destination),true);
+    ASSERT_EQ(graphVide->arcExiste(source, destination), true);
 }
 TEST_F(Studs, GivenAGraphWithAnArcWhenEnleverAnArchThenArcShouldBeRemove)
 {
@@ -202,106 +201,97 @@ TEST_F(Studs, GivenAGraphWithANArcWhenEnleverAnArchOutOfIndexThenItShouldThrowEr
     ASSERT_EQ(graph_1_Arc->getNombreArcs(), 1);
 }
 
-TEST_F(Studs2,ArcExisteInGraph)
+TEST_F(Studs2, ArcExisteInGraph)
 {
-    ASSERT_FALSE(graph_0_Arc->arcExiste(source1,destination1));
+    ASSERT_FALSE(graph_0_Arc->arcExiste(source1, destination1));
 
-    ASSERT_EQ(graph_1_Arc->arcExiste(source1,destination1),true);
+    ASSERT_TRUE(graph_1_Arc->arcExiste(source1, destination1));
 
-    ASSERT_EQ(graph_1_Arc->arcExiste(source1,destination1),true);
-    ASSERT_EQ(graph_2_Arc->arcExiste(source2,destination2),true);
+    ASSERT_TRUE(graph_1_Arc->arcExiste(source1, destination1));
+    ASSERT_TRUE(graph_2_Arc->arcExiste(source2, destination2));
 
-    ASSERT_EQ(graph_n_Arc->arcExiste(source1,destination1),true);
-    ASSERT_EQ(graph_n_Arc->arcExiste(source2,destination2),true);
-    ASSERT_EQ(graph_n_Arc->arcExiste(source3,destination3),true);
-    
+    ASSERT_TRUE(graph_n_Arc->arcExiste(source1, destination1));
+    ASSERT_TRUE(graph_n_Arc->arcExiste(source2, destination2));
+    ASSERT_TRUE(graph_n_Arc->arcExiste(source3, destination3));
 }
 
-TEST_F(Studs2,ArcExisteNotInGraph)
+TEST_F(Studs2, ArcExisteNotInGraph)
 {
-    ASSERT_EQ(graph_1_Arc->arcExiste(source3,destination3),false);
-    ASSERT_EQ(graph_2_Arc->arcExiste(source3,destination3),false);
-    ASSERT_EQ(graph_2_Arc->arcExiste(source4,destination4),false);
-    ASSERT_EQ(graph_n_Arc->arcExiste(source5,destination5),false);
+    ASSERT_FALSE(graph_1_Arc->arcExiste(source3, destination3));
+    ASSERT_FALSE(graph_2_Arc->arcExiste(source3, destination3));
+    ASSERT_FALSE(graph_2_Arc->arcExiste(source4, destination4));
+    ASSERT_FALSE(graph_n_Arc->arcExiste(source5, destination5));
 
-    ASSERT_EQ(graph_2_Arc->arcExiste(source3,destination3),false);
-    ASSERT_EQ(graph_2_Arc->arcExiste(source4,destination4),false);
-    ASSERT_EQ(graph_n_Arc->arcExiste(source5,destination5),false);
-  
-    ASSERT_EQ(graph_n_Arc->arcExiste(source5,destination5),false);
-    
-    
+    ASSERT_FALSE(graph_2_Arc->arcExiste(source3, destination3));
+    ASSERT_FALSE(graph_2_Arc->arcExiste(source4, destination4));
+    ASSERT_FALSE(graph_n_Arc->arcExiste(source5, destination5));
 
+    ASSERT_FALSE(graph_n_Arc->arcExiste(source5, destination5));
 }
 
-TEST_F(Studs2,ArcExistOutOfIndex)
+TEST_F(Studs2, ArcExistOutOfIndex)
 {
-    size_t source_test=12;
-    size_t destination_test=15;
-    
-    ASSERT_THROW(graph_1_Arc->arcExiste(source_test,destination_test),logic_error);
+    size_t source_test = 12;
+    size_t destination_test = 15;
 
-    ASSERT_THROW(graph_2_Arc->arcExiste(source_test,destination_test),logic_error);
-    
-    ASSERT_THROW(graph_n_Arc->arcExiste(source_test,destination_test),logic_error);
-    
+    ASSERT_THROW(graph_1_Arc->arcExiste(source_test, destination_test), logic_error);
 
+    ASSERT_THROW(graph_2_Arc->arcExiste(source_test, destination_test), logic_error);
+
+    ASSERT_THROW(graph_n_Arc->arcExiste(source_test, destination_test), logic_error);
 }
-
 
 TEST_F(Studs2, Studs2_listerSommetsAdjacents_Test)
 {
     std::vector<size_t> vecteur_test;
-    size_t sommet_test_1=1;
-    size_t sommet_test_2=2;
+    size_t sommet_test_1 = 1;
+    size_t sommet_test_2 = 2;
     vecteur_test.push_back(sommet_test_2);
 
-    ASSERT_EQ(graph_1_Arc->listerSommetsAdjacents(sommet_test_1),vecteur_test);
+    ASSERT_EQ(graph_1_Arc->listerSommetsAdjacents(sommet_test_1), vecteur_test);
 }
 
 TEST_F(Studs2, Studs2_getNomSommet_Test)
 
-{   graph_1_Arc->nommer(0,"Test_1");
-    graph_1_Arc->nommer(1,"Test_2");
-    graph_1_Arc->nommer(2,"Test_3");
-    graph_1_Arc->nommer(3,"Test_4");
-    ASSERT_EQ(graph_1_Arc->getNomSommet(0),"Test_1");
-    ASSERT_EQ(graph_1_Arc->getNomSommet(1),"Test_2");
-    ASSERT_EQ(graph_1_Arc->getNomSommet(2),"Test_3");
-    ASSERT_EQ(graph_1_Arc->getNomSommet(3),"Test_4");
-
+{
+    graph_1_Arc->nommer(0, "Test_1");
+    graph_1_Arc->nommer(1, "Test_2");
+    graph_1_Arc->nommer(2, "Test_3");
+    graph_1_Arc->nommer(3, "Test_4");
+    ASSERT_EQ(graph_1_Arc->getNomSommet(0), "Test_1");
+    ASSERT_EQ(graph_1_Arc->getNomSommet(1), "Test_2");
+    ASSERT_EQ(graph_1_Arc->getNomSommet(2), "Test_3");
+    ASSERT_EQ(graph_1_Arc->getNomSommet(3), "Test_4");
 }
 
-TEST_F(Studs2,Studs2_getNumeroSommet_Test)
+TEST_F(Studs2, Studs2_getNumeroSommet_Test)
 {
-    graph_2_Arc->nommer(0,"Test_1");
-    graph_2_Arc->nommer(1,"Test_2");
-    graph_2_Arc->nommer(2,"Test_3");
-    graph_2_Arc->nommer(3,"Test_4");
-    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_1"),0);
-    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_2"),1);
-    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_3"),2);
-    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_4"),3);
-  
+    graph_2_Arc->nommer(0, "Test_1");
+    graph_2_Arc->nommer(1, "Test_2");
+    graph_2_Arc->nommer(2, "Test_3");
+    graph_2_Arc->nommer(3, "Test_4");
+    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_1"), 0);
+    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_2"), 1);
+    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_3"), 2);
+    ASSERT_EQ(graph_2_Arc->getNumeroSommet("Test_4"), 3);
 }
 
-TEST_F(Studs2,Studs2_getNombreSommets_TEST)
+TEST_F(Studs2, Studs2_getNombreSommets_TEST)
 {
-    ASSERT_EQ(graph_0_Arc->getNombreSommets(),nb_sommet_graph);
-    ASSERT_EQ(graph_1_Arc->getNombreSommets(),nb_sommet_graph);
-    ASSERT_EQ(graph_2_Arc->getNombreSommets(),nb_sommet_graph);
-    ASSERT_EQ(graph_n_Arc->getNombreSommets(),nb_sommet_graph);
-
+    ASSERT_EQ(graph_0_Arc->getNombreSommets(), nb_sommet_graph);
+    ASSERT_EQ(graph_1_Arc->getNombreSommets(), nb_sommet_graph);
+    ASSERT_EQ(graph_2_Arc->getNombreSommets(), nb_sommet_graph);
+    ASSERT_EQ(graph_n_Arc->getNombreSommets(), nb_sommet_graph);
 }
 
-TEST_F(Studs2,Studs2_getNombreArcs_TEST)
+TEST_F(Studs2, Studs2_getNombreArcs_TEST)
 {
-    size_t arc_test_1=0;
-    size_t arc_test_2=2;
-    size_t arc_test_3=2;
-    size_t arc_test_4=4;
-    ASSERT_EQ(graph_0_Arc->getNombreArcs(),arc_test_1);
-    ASSERT_EQ(graph_1_Arc->getNombreArcs(),arc_test_2);
-    ASSERT_EQ(graph_2_Arc->getNombreArcs(),arc_test_3);
-    ASSERT_EQ(graph_n_Arc->getNombreArcs(),arc_test_4);
+    size_t arc_test_1 = 0;
+    size_t arc_test_2 = 2;
+    size_t arc_test_3 = 2;
+    size_t arc_test_4 = 4;
+    ASSERT_EQ(graph_0_Arc->getNombreArcs(), arc_test_1);
+    ASSERT_EQ(graph_1_Arc->getNombreArcs(), arc_test_2);
+    ASSERT_EQ(graph_2_Arc->getNombreArcs(), arc_test_3);
+    ASSERT_EQ(graph_n_Arc->getNombreArcs(), arc_test_4);
 }
